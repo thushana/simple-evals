ca3147c8776516d7d4dcd870188ac1ab1fcedb9b
Thushan Amarasiriwardena <thushan@thushan.net>
2025-07-22 10:42:05 -0400

    BACKEND - Fixes some CORS issues in "prod"

--- college_board_eval/web/backend/main.py
+++ college_board_eval/web/backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["http://localhost:4173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
